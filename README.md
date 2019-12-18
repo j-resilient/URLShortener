8433bae9c8a3b43244d5032f704dafce6964f912
J Resilient <j.resilient.1@gmail.com>
2019-12-18 10:38:31 -0500

    Add ability to create and persist shortened urls

--- README.md
+++ README.md
@@ -4,5 +4,7 @@ A command line URL shortener. Stores users by email.
 ## Technology Stack  
 Ruby 2.5, Rails 5.2.4, Postgresql
 ## How to use
+To create a new ShortenedUrl entry on the command line run:  
+ShortenedUrl.create_for_user_and_long_url!(user_id, long_url)
 
 
---
+++ app/models/shortened_url.rb
@@ -0,0 +1,45 @@
+# == Schema Information
+#
+# Table name: shortened_urls
+#
+#  id         :bigint           not null, primary key
+#  long_url   :string           not null
+#  short_url  :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :integer          not null
+#
+# Indexes
+#
+#  index_shortened_urls_on_short_url             (short_url) UNIQUE
+#  index_shortened_urls_on_user_id_and_long_url  (user_id,long_url) UNIQUE
+#
+
+class ShortenedUrl < ApplicationRecord
+    validates :long_url, :user_id, presence: true
+    validates :long_url, uniqueness: { 
+        scope: :user_id,
+        message: 'You have already shortened this URL.'
+    }
+
+    belongs_to :submitter,
+        primary_key: :id,
+        foreign_key: :user_id,
+        class_name: 'User'
+
+    def self.create_for_user_and_long_url!(user_id, long_url)
+        ShortenedUrl.create! ({
+            user_id: user_id,
+            long_url: long_url,
+            short_url: ShortenedUrl.random_code
+        })
+    end
+
+    def self.random_code
+        code = SecureRandom.urlsafe_base64(16)
+        while ShortenedUrl.exists?(:short_url => code)
+            code = SecureRandom.urlsafe_base64
+        end
+        code
+    end
+end
